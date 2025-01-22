5ba170d53b333dccc8f91569f5e25697eab57387
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-01-22 16:05:58 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Klanke Min Side Microfrontend
+# Mine klager - Min side microfrontend
 
 ## Development
 
 1. Install [Bun](https://bun.sh)
 2. Install dependencies: `bun i`
 3. Start development server: `bun start`
+4. Open [localhost:3001](http://localhost:3001)
--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,17 +1,5 @@
-import { init, track } from '@amplitude/analytics-browser';
+import { getAmplitudeInstance } from '@navikt/nav-dekoratoren-moduler';
 
-export const initAmplitude = () => {
-  init('default', undefined, {
-    useBatch: true,
-    serverUrl: 'https://amplitude.nav.no/collect-auto',
-    ingestionMetadata: {
-      sourceName: window.location.toString(),
-    },
-  });
-};
+const logger = getAmplitudeInstance('dekoratoren');
 
-//TODO: komponent må fylles ut med et identifiserende navn for microfrontend
-//Default er at navnet på appen blir benyttet ved replace av "klanke-min-side-microfrontend"
-export const logNavigereEvent = () => {
-  track('navigere', { komponent: 'klanke-min-side-microfrontend' });
-};
+export const logNavigereEvent = () => logger('navigere', { komponent: 'mine-klager-microfrontend' });
